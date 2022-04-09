20bae69052ee29463b270d0e80334ea174aadd38
test <test@gmail.com>
2022-04-09 12:10:17 +0700

    initialization, adding files

---
+++ src/main/java/Client.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.CompletableFuture;
+
+class Client {
+    public CompletableFuture<HackerRank.Tutorial> getFirstTutorial() {
+        return new CompletableFuture<>();
+    }
+    public CompletableFuture<HackerRank.Tutorial> getSecondTutorial() {
+        return new CompletableFuture<>();
+    }
+
+
+}
